Fix the in-place operation error in gradient computation

BEATs/backbone.py
@@ -113,7 +113,9 @@ class TransformerEncoder(nn.Module):
 
         x_conv = self.pos_conv(x.transpose(1, 2))
         x_conv = x_conv.transpose(1, 2)
-        x += x_conv
+        #x += x_conv
+        # Avoid the inplace operation which messes up the gradients
+        x = x.clone() + x_conv
 
         if not self.layer_norm_first:
             x = self.layer_norm(x)
@@ -122,7 +124,7 @@ class TransformerEncoder(nn.Module):
 
         # B x T x C -> T x B x C
         x = x.transpose(0, 1)
-
+        
         layer_results = []
         z = None
         if tgt_layer is not None:

fine_tune/transferLearning.py
@@ -74,13 +74,10 @@ class BEATsTransferLearningModel(pl.LightningModule):
         # Mean pool the second layer 
         x = x.mean(dim=1)
 
-        # Softmax the output
-        x = F.softmax(x, dim=-1)
-
         return x
 
     def loss(self, lprobs, labels):
-        self.loss_func = nn.NLLLoss() # CrossEntropy loss if not log_softmax
+        self.loss_func = nn.CrossEntropyLoss()
         return self.loss_func(lprobs, labels)
         
     def training_step(self, batch, batch_idx):
@@ -109,7 +106,7 @@ class BEATsTransferLearningModel(pl.LightningModule):
         self.log("val_acc", self.valid_acc(y_probs, y_true), prog_bar=True)
 
     def configure_optimizers(self):
-        optimizer = optim.Adam([
+        optimizer = optim.AdamW([
             {'params': self.beats.parameters()},
             {'params': self.fc.parameters()}
             ], lr=self.lr)