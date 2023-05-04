7f49a5ac13bde4298c8f4840bdf201ab927718e7
Femke Gelderblom <femke.gelderblom@sintef.no>
2023-05-04 13:37:58 +0000

    [FEAT] evaluation obtains correct preprocessed data

--- evaluate/evaluateDCASE.py
+++ evaluate/evaluateDCASE.py
@@ -5,8 +5,9 @@ import numpy as np
 import pandas as pd
 import glob
 import os
-
+import hashlib
 import yaml
+import json
 from yaml import FullLoader
 
 from sklearn.metrics import accuracy_score, recall_score, f1_score
@@ -24,8 +25,8 @@ import pytorch_lightning as pl
 
 from callbacks.callbacks import MilestonesFinetuning
 
-def to_dataframe(features, labels):
 
+def to_dataframe(features, labels):
     # Load the saved array and map the features and labels into a single dataframe
     input_features = np.load(features)
     labels = np.load(labels)
@@ -34,7 +35,17 @@ def to_dataframe(features, labels):
 
     return df
 
-def train_model(model_class=ProtoBEATsModel, datamodule_class=DCASEDataModule, milestones=[10,20,30], max_epochs=15, enable_model_summary=False, num_sanity_val_steps=0, seed=42, pretrained_model=None):
+
+def train_model(
+    model_class=ProtoBEATsModel,
+    datamodule_class=DCASEDataModule,
+    milestones=[10, 20, 30],
+    max_epochs=15,
+    enable_model_summary=False,
+    num_sanity_val_steps=0,
+    seed=42,
+    pretrained_model=None,
+):
     # create the lightning trainer object
     trainer = pl.Trainer(
         max_epochs=max_epochs,
@@ -43,9 +54,9 @@ def train_model(model_class=ProtoBEATsModel, datamodule_class=DCASEDataModule, m
         deterministic=True,
         gpus=1,
         auto_select_gpus=True,
-        callbacks=[pl.callbacks.LearningRateMonitor(logging_interval='step')],
-        default_root_dir='logs/',
-        logger=pl.loggers.TensorBoardLogger('logs/', name='my_model')
+        callbacks=[pl.callbacks.LearningRateMonitor(logging_interval="step")],
+        default_root_dir="logs/",
+        logger=pl.loggers.TensorBoardLogger("logs/", name="my_model"),
     )
 
     # create the model object
@@ -60,8 +71,8 @@ def train_model(model_class=ProtoBEATsModel, datamodule_class=DCASEDataModule, m
 
     return model
 
-def training(pretrained_model, custom_datamodule, max_epoch, milestones=[10,20,30]):
 
+def training(pretrained_model, custom_datamodule, max_epoch, milestones=[10, 20, 30]):
     model = train_model(
         ProtoBEATsModel,
         custom_datamodule,
@@ -70,23 +81,28 @@ def training(pretrained_model, custom_datamodule, max_epoch, milestones=[10,20,3
         enable_model_summary=False,
         num_sanity_val_steps=0,
         seed=42,
-        pretrained_model=pretrained_model
+        pretrained_model=pretrained_model,
     )
 
     return model
 
-def get_proto_coordinates(model, support_data, support_labels, n_way):
 
+def get_proto_coordinates(model, support_data, support_labels, n_way):
     z_supports, _ = model.get_embeddings(support_data, padding_mask=None)
 
     # Get the coordinates of the NEG and POS prototypes
-    prototypes = model.get_prototypes(z_support= z_supports, support_labels= support_labels, n_way=n_way)
+    prototypes = model.get_prototypes(
+        z_support=z_supports, support_labels=support_labels, n_way=n_way
+    )
 
     # Return the coordinates of the prototypes
     return prototypes
 
-def predict_labels_query(model, queryloader, prototypes, l_segments, frame_shift, overlap):
-    """ 
+
+def predict_labels_query(
+    model, queryloader, prototypes, tensor_length, frame_shift, overlap
+):
+    """
     - l_segment to know the length of the segment
     - offset is the position of the end of the last support sample
     """
@@ -101,7 +117,6 @@ def predict_labels_query(model, queryloader, prototypes, l_segments, frame_shift
     ends = []
 
     for i, data in enumerate(tqdm(queryloader)):
-
         # Get the embeddings for the query
         feature, label = data
         feature = feature.to("cuda")
@@ -111,16 +126,18 @@ def predict_labels_query(model, queryloader, prototypes, l_segments, frame_shift
         # We multiply by 100 to get the time in seconds
         if i == 0:
             begin = i / 1000
-            end = l_segments * frame_shift / 1000
+            end = tensor_length * frame_shift / 1000
         else:
-            begin = i * l_segments * frame_shift * overlap / 1000
-            end = begin + l_segments * frame_shift / 1000
+            begin = i * tensor_length * frame_shift * overlap / 1000
+            end = begin + tensor_length * frame_shift / 1000
 
         # Get the scores:
         classification_scores = calculate_distance(q_embedding, prototypes)
 
         # Get the labels (either POS or NEG):
-        predicted_labels = torch.max(classification_scores, 0)[1] # The dim where the distance to prototype is stored is 1
+        predicted_labels = torch.max(classification_scores, 0)[
+            1
+        ]  # The dim where the distance to prototype is stored is 1
 
         # Return the labels, begin and end of the detection
         pred_labels.append(predicted_labels)
@@ -133,27 +150,30 @@ def predict_labels_query(model, queryloader, prototypes, l_segments, frame_shift
 
     return pred_labels, labels, begins, ends
 
+
 def euclidean_distance(x1, x2):
     return torch.sqrt(torch.sum((x1 - x2) ** 2, dim=1))
 
-def calculate_distance(z_query, z_proto):
 
+def calculate_distance(z_query, z_proto):
     # Compute the euclidean distance from queries to prototypes
     dists = []
     for q in z_query:
         q_dists = euclidean_distance(q.unsqueeze(0), z_proto)
-        dists.append(q_dists.unsqueeze(0)) # Contrary to prototraining I need to add a dimension to store the 
+        dists.append(
+            q_dists.unsqueeze(0)
+        )  # Contrary to prototraining I need to add a dimension to store the
     dists = torch.cat(dists, dim=0)
 
-    # We drop the last dimension without changing the gradients 
+    # We drop the last dimension without changing the gradients
     dists = dists.mean(dim=2).squeeze()
 
     scores = -dists
 
     return scores
 
-def compute_scores(predicted_labels, gt_labels):
 
+def compute_scores(predicted_labels, gt_labels):
     acc = accuracy_score(gt_labels, predicted_labels)
     recall = recall_score(gt_labels, predicted_labels)
     f1score = f1_score(gt_labels, predicted_labels)
@@ -162,31 +182,41 @@ def compute_scores(predicted_labels, gt_labels):
     print(f"Recall: {recall}")
     print(f"F1 score: {f1score}")
 
+
 def write_results(filename, predicted_labels, begins, ends):
+    name = np.repeat(filename, len(begins))
 
-    name = np.repeat(filename,len(begins))
+    df_out = pd.DataFrame(
+        {
+            "Audiofilename": name,
+            "Starttime": begins,
+            "Endtime": ends,
+            "PredLabels": predicted_labels,
+        }
+    )
 
-    df_out = pd.DataFrame({'Audiofilename':name,
-                           'Starttime':begins, 
-                           'Endtime':ends, 
-                           'PredLabels':predicted_labels})
-    
     return df_out
 
-def main(cfg, meta_df, support_spectrograms, support_labels, query_spectrograms, query_labels):
 
+def main(
+    cfg, meta_df, support_spectrograms, support_labels, query_spectrograms, query_labels
+):
     # Get the filename and the frame_shift for the particular file
     filename = os.path.basename(support_spectrograms).split("data_")[1].split(".")[0]
     frame_shift = meta_df.loc[filename, "frame_shift"]
 
     print("[INFO] PROCESSING {}".format(filename))
+    # check labels and spectograms all from same file
+    assert filename in support_labels
+    assert filename in query_spectrograms
+    assert filename in query_labels
 
     df_support = to_dataframe(support_spectrograms, support_labels)
     custom_dcasedatamodule = DCASEDataModule(data_frame=df_support)
     label_dic = custom_dcasedatamodule.get_label_dic()
 
     # Train the model with the support data
-    print ("[INFO] TRAINING THE MODEL FOR {}".format(filename))
+    print("[INFO] TRAINING THE MODEL FOR {}".format(filename))
 
     model = training(cfg["model_path"], custom_dcasedatamodule, max_epoch=1)
 
@@ -198,15 +228,27 @@ def main(cfg, meta_df, support_spectrograms, support_labels, query_spectrograms,
     ### Get the query dataset ###
     df_query = to_dataframe(query_spectrograms, query_labels)
     queryLoader = AudioDatasetDCASE(df_query, label_dict=label_dic)
-    queryLoader = DataLoader(queryLoader, batch_size=1) # KEEP BATCH SIZE OF 1 TO GET BEGIN AND END OF SEGMENT
+    queryLoader = DataLoader(
+        queryLoader, batch_size=1
+    )  # KEEP BATCH SIZE OF 1 TO GET BEGIN AND END OF SEGMENT
 
     # Get the results
     print("[INFO] DOING THE PREDICTION FOR {}".format(filename))
 
-    predicted_labels, labels, begins, ends = predict_labels_query(model, queryLoader, prototypes, l_segments=cfg["l_segments"], frame_shift=frame_shift, overlap=cfg["overlap"])
+    predicted_labels, labels, begins, ends = predict_labels_query(
+        model,
+        queryLoader,
+        prototypes,
+        tensor_length=cfg["tensor_length"],
+        frame_shift=frame_shift,
+        overlap=cfg["overlap"],
+    )
 
-    # Compute the scores for the analysed file -- just as information    
-    compute_scores(predicted_labels=predicted_labels.to('cpu').numpy(), gt_labels=labels.to('cpu').numpy())
+    # Compute the scores for the analysed file -- just as information
+    compute_scores(
+        predicted_labels=predicted_labels.to("cpu").numpy(),
+        gt_labels=labels.to("cpu").numpy(),
+    )
 
     # Get the results in a dataframe
     df_result = write_results(filename, predicted_labels, begins, ends)
@@ -219,7 +261,9 @@ def main(cfg, meta_df, support_spectrograms, support_labels, query_spectrograms,
     df_result["PredLabels"] = df_result["PredLabels"].map(label_dict_inv)
 
     # Filter only the POS results
-    result_POS = df_result[df_result["PredLabels"] == "POS"].drop(["PredLabels"], axis=1)
+    result_POS = df_result[df_result["PredLabels"] == "POS"].drop(
+        ["PredLabels"], axis=1
+    )
 
     # Return the dataset
     print("[INFO] {} PROCESSED".format(filename))
@@ -227,48 +271,120 @@ def main(cfg, meta_df, support_spectrograms, support_labels, query_spectrograms,
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--config",
-                        help="Path to the config file",
-                        required=False,
-                        default="./evaluate/config_evaluation.yaml",
-                        type=str
+    parser.add_argument(
+        "--config",
+        help="Path to the config file",
+        required=False,
+        default="./evaluate/config_evaluation.yaml",
+        type=str,
     )
-    
+
     cli_args = parser.parse_args()
 
+    # Get evalution config
     with open(cli_args.config) as f:
         cfg = yaml.load(f, Loader=FullLoader)
 
+    # Get training config
+    training_config_path = os.path.join(
+        os.path.dirname(os.path.dirname(cfg["model_path"])), "config.yaml"
+    )
+    with open(training_config_path) as f:
+        cfg_trainer = yaml.load(f, Loader=FullLoader)
+
+    # Get correct paths to dataset
+    data_hp = cfg_trainer["data"]["init_args"]
+    my_hash_dict = {
+        "resample": data_hp["resample"],
+        "denoise": data_hp["denoise"],
+        "normalize": data_hp["normalize"],
+        "frame_length": data_hp["frame_length"],
+        "tensor_length": data_hp["tensor_length"],
+    }
+    assert cfg["status"] == "validate" or cfg["status"] == "test"
+    my_hash_dict["set_type"] = (
+        "Validation_Set" if cfg["status"] == "validate" else "Evaluation_Set"
+    )
+    if data_hp["resample"]:
+        my_hash_dict["tartget_fs"] = data_hp["target_fs"]
+    hash_dir_name = hashlib.sha1(
+        json.dumps(my_hash_dict, sort_keys=True).encode()
+    ).hexdigest()
+
+    # ensure cfg of evaluation knows preprocessing tensor_length
+    cfg["tensor_length"] = data_hp["tensor_length"]
+
+    # get meta, support and query paths
+    support_data_path = os.path.join(
+        "/data/DCASEfewshot",
+        cfg["status"],
+        hash_dir_name,
+        "audio",
+        "support_data_*.npz",
+    )
+    support_labels_path = os.path.join(
+        "/data/DCASEfewshot",
+        cfg["status"],
+        hash_dir_name,
+        "audio",
+        "support_labels_*.npy",
+    )
+    query_data_path = os.path.join(
+        "/data/DCASEfewshot", cfg["status"], hash_dir_name, "audio", "query_data_*.npz"
+    )
+    query_labels_path = os.path.join(
+        "/data/DCASEfewshot",
+        cfg["status"],
+        hash_dir_name,
+        "audio",
+        "query_labels_*.npy",
+    )
+    meta_df_path = os.path.join(
+        "/data/DCASEfewshot", cfg["status"], hash_dir_name, "audio", "meta.csv"
+    )
+
     # List all the SUPPORT files (both labels and spectrograms)
-    support_all_spectrograms = glob.glob(cfg["support_data_path"], recursive=True)
-    support_all_labels = glob.glob(cfg["support_labels_path"], recursive=True)
+    support_all_spectrograms = glob.glob(support_data_path, recursive=True)
+    support_all_labels = glob.glob(support_labels_path, recursive=True)
 
     # List all the QUERY files
-    query_all_spectrograms = glob.glob(cfg["query_data_path"], recursive=True)
-    query_all_labels = glob.glob(cfg["query_labels_path"], recursive=True)
+    query_all_spectrograms = glob.glob(query_data_path, recursive=True)
+    query_all_labels = glob.glob(query_labels_path, recursive=True)
+
+    # ensure lists are ordered the same
+    support_all_spectrograms.sort()
+    support_all_labels.sort()
+    query_all_spectrograms.sort()
+    query_all_labels.sort()
 
     # Open the meta.csv containing the frame_shift for each file
-    meta_df = pd.read_csv(cfg["meta_df_path"], names = ["frame_shift", "filename"]).set_index('filename')
+    meta_df = pd.read_csv(meta_df_path, names=["frame_shift", "filename"]).set_index(
+        "filename"
+    )
 
     # Dataset to store all the results
     results = pd.DataFrame()
 
     # Run the main script
-    for support_spectrograms, support_labels, query_spectrograms, query_labels in zip(support_all_spectrograms, support_all_labels, query_all_spectrograms, query_all_labels):
+    for support_spectrograms, support_labels, query_spectrograms, query_labels in zip(
+        support_all_spectrograms,
+        support_all_labels,
+        query_all_spectrograms,
+        query_all_labels,
+    ):
+        result = main(
+            cfg,
+            meta_df,
+            support_spectrograms,
+            support_labels,
+            query_spectrograms,
+            query_labels,
+        )
 
-        result = main(cfg, 
-            meta_df, 
-            support_spectrograms, 
-            support_labels, 
-            query_spectrograms, 
-            query_labels)
-        
         results = results.append(result)
 
     # Return the final product
-    csv_path = os.path.join(cfg["save_dir"],'eval_out.csv')
-    results.to_csv(csv_path,index=False)
-
+    csv_path = os.path.join(cfg["save_dir"], "eval_out.csv")
+    results.to_csv(csv_path, index=False)
