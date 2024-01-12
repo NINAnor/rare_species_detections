ccb4636584184d5831b707d214b7bd322457255c
Benjamin Cretois <benjamin.cretois@nina.no>
2024-01-12 14:32:30 +0100

    [UPDATE] dcase_setup

--- dcase_setup.sh
+++ dcase_setup.sh
@@ -33,6 +33,14 @@ MODEL_FOLDER=$BASE_FOLDER/model/BEATs
 mkdir -p $MODEL_FOLDER
 wget -O "$MODEL_FOLDER/BEATs_iter3_plus_AS2M.pt" "https://valle.blob.core.windows.net/share/BEATs/BEATs_iter3_plus_AS2M.pt?sv=2020-08-04&st=2023-03-01T07%3A51%3A05Z&se=2033-03-02T07%3A51%3A00Z&sr=c&sp=rl&sig=QJXmSJG9DbMKf48UDIU1MfzIro8HQOf3sqlNXiflY1I%3D" 
 
+###########################
+# Download the PANN model #
+###########################
+
+CHECKPOINT_PATH="$BASE_FOLDER/model/PANN"
+mkdir -p $CHECKPOINT_PATH
+wget -O $CHECKPOINT_PATH/Cnn14_mAP=0.431.pth https://zenodo.org/record/3987831/files/Cnn14_mAP%3D0.431.pth?download=1
+
 #############################################################
 # Download the development set (i.e. training + validation) #
 #############################################################
